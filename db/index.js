cd677a6a3f12f1f320993ce880e86e415023a60f
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-06-25 14:52:37 +0530

    mongoDB connection created

---
+++ db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const DB_NAME = "todo-list"
+
+const connectDB = async() => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MONGODB CONNECTION INITIATED : ${connectionInstance.connection.port}`)
+
+    }
+    catch(error){
+        console.log('MONGODB CONNECTION ERROR', error)
+    }
+}
+
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import express from 'expresss';
+import express from 'express';
 import 'dotenv/config'
-require('dotenv').config()
+import connectDB from './db/index.js'
+
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req,res) => {
+    res.send("sever is running!")
+})
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
+
+
+
+
+
 
 
 
