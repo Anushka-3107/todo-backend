6a9e1aada4c9975e144e0ad848eda77055637527
anushka3107-bit <bhardwajanushka180@gmail.com>
2025-07-14 14:40:15 +0530

    bug fixes in backend

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,109 +1,27 @@
-// import { useState} from "react"
 
-// const Todo = () => {
-
-//   const [todos,setTodos] = useState("");
-//   const [isChecked,setIsChecked ]= useState(false);
-//   const [todosList,setTodosList] = useState([]);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     console.log("Todos: ", todos, "Checked:" , isChecked);
-
-//     if(todos.trim() === "") return;
-
-//     const newTodos = {
-//       id:Date.now(),
-//       text: todos,
-//       completed: isChecked,
-//     }
-
-//     setTodos("")
-//     setTodosList([...todos,newTodos]);
-//     setIsChecked(false);
-//   }
-
-
-//   return (
-//     <div className="flex flex-col text-3xl justify-center items-center">
-//         <h1 className="text-3xl text-purple-600 font-bold text-center">TODO it!</h1>
-
-//         <div className="flex justify-center items-center m-2 p-2 text-red-300">
-          
-//            <form
-//            onSubmit={handleSubmit}
-//            className="flex items-center gap-4 m-2 p-4 text-red-400"
-//            >
-           
-//             <input 
-//             type="text"
-//             placeholder="enter your todos" 
-//             className="outline-solid focus:outline-none focus:ring-2 focus:ring-red-400 px-2 py-1 text-base rounded"
-//             value={todos}
-//             onChange={(e) => setTodos(e.target.value)}
-//             />
-
-
-//             <label className="flex items-center gap-1 text-base">
-//            <input type="checkbox"
-//             checked={isChecked}
-//             onChange={() => setIsChecked(!isChecked)}
-//            />
-//              Mask as Complete
-//            </label>
-
-
-//            <button
-//            type="submit"
-//            className="bg-purple-600 text-white px-3 py-1 rounded hover:bg-purple-400"
-//            >
-//             ADD
-//            </button>
-           
-//            </form>
-
-//            <div className="bg-red-900">
-//            {
-//             todosList.length === 0 ? (
-//               <p className="text-gray-300 text-lg text-center">No Todos yet</p>
-//             ):(
-//               <ul>
-//                 {
-//                   todosList.map((todo) => (
-//                     <li
-//                     key={todo.id}
-//                     // className={`flex items-center justify-between px-4 py-2 border rounded ${
-//                     // todo.completed ? "bg-green-100": "bg-white"}`}
-//                     >
-//                     <span
-//                     className={`text-lg 
-//                     ${
-//                     todo.completed ? "line-through text-gray-500" : ""}
-//                     `}
-//                     >
-//                     {todo.text}
-//                     </span>
-
-//                     </li>
-//                   ))}
-//               </ul>
-//             )}
-//            </div>
-//         </div>
-//     </div>
-//   )
-// }
-// export default Todo
-
-
-
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Todo = () => {
   const [todoInput, setTodoInput] = useState("");
   const [isChecked, setIsChecked] = useState(false);
   const [todoList, setTodoList] = useState([]);
+
+  const API_URL = "http://localhost:8000/api/todos";
+
+  const fetchTodos = async () => {
+    try {
+      const res = await fetch(API_URL);
+      console.log("Response" , res)
+      const data = await res.json();
+      setTodoList(data);
+
+    } catch (err) {
+      console.error("error fetching todos: ", err);
+    }
+  }
+  useEffect(()=> {
+    fetchTodos();
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -116,9 +34,19 @@ const Todo = () => {
       completed: isChecked,
     };
 
+    try {
+
+      const res = fetch(API_URL)
+
     setTodoList([...todoList, newTodo]);
     setTodoInput("");
     setIsChecked(false);
+      
+    } catch (err) {
+      console.error("failed to add todo:", err)
+    }
+
+    
   };
 
   const toggleComplete = (id) => {
@@ -189,7 +117,7 @@ const Todo = () => {
 
                  <button
                   onClick={() => deleteTodo(todo.id)}
-                  className="text-sm bg-gray-300 px-2 py-1 rounded hover:bg-gray-400"
+                  className="text-sm bg-red-600 px-2 py-1 rounded hover:bg-gray-400"
                 >
                   Delete
                 </button>
